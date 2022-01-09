51e51c986f0a5f861b6e825abf9005672b6b93ab
sepehr javid <javid.sepehr77@gmail.com>
2022-01-09 13:06:52 +0200

    Exercise 4.6

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -59,3 +59,42 @@ describe('favorite blog', () => {
         })
     })
 })
+
+
+describe('most blogs', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17a6',
+            title: 'How to excel at security',
+            author: 'Sepehr Javid',
+            url: 'sep.com',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234f17a6',
+            title: 'How to excel at security 2',
+            author: 'Sepehr Javid',
+            url: 'sep.com',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('sepehr author should be returned', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        expect(result).toEqual({
+            author: 'Sepehr Javid',
+            blogs: 2
+        })
+    })
+})
+
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,8 +10,31 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog);
 }
 
+const mostBlogs = (blogs) => {
+    var authorBlogCount = {}
+    blogs.forEach((blog) => {
+        if (authorBlogCount.hasOwnProperty(blog.author)) {
+            authorBlogCount[blog.author]++
+        } else {
+            authorBlogCount[blog.author] = 1
+        }
+    })
+
+
+    var mostActiveAuthor = Object.keys(authorBlogCount).reduce(
+        (mostActiveAuthor, author) => authorBlogCount[author] > authorBlogCount[mostActiveAuthor] ? author : mostActiveAuthor,
+        Object.keys(authorBlogCount)[0]);
+
+
+    return {
+        author: mostActiveAuthor,
+        blogs: authorBlogCount[mostActiveAuthor]
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
