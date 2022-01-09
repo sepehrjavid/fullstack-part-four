51e95bea147d7a18f5dd0f000f90d02c38882e02
sepehr javid <javid.sepehr77@gmail.com>
2022-01-09 12:36:05 +0200

    Exercise 4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -25,3 +25,37 @@ describe('total likes', () => {
         expect(result).toBe(5)
     })
 })
+
+
+describe('favorite blog', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17a6',
+            title: 'How to excel at security',
+            author: 'Sepehr Javid',
+            url: 'sep.com',
+            likes: 7,
+            __v: 0
+        }
+    ]
+
+    test('the sepehr blog should be returned', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual({
+            _id: '5a422aa71b54a676234d17a6',
+            title: 'How to excel at security',
+            author: 'Sepehr Javid',
+            url: 'sep.com',
+            likes: 7,
+            __v: 0
+        })
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((previous, current) => previous + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog);
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
